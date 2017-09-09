d3aad491ce38078f38f2f331ae9380d6878f5501
phearom neang <phearom.neang@outlook.com>
2017-09-09 21:29:16 +0700

    add web api 2 crud

---
+++ MusicStore/API/AlbumController.cs
@@ -0,0 +1,86 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicStore.API
+{
+    //[RoutePrefix("api/album")]
+    public class AlbumController : ApiController
+    {
+        private Repository<Album> album = new Repository<Album>();
+
+        [HttpGet]
+        //[Route("getall")]
+        public List<Album> GetAllAlbum()
+        {
+            var data = album.GetAll();
+            return data;
+        }
+
+        [HttpGet]
+        //[Route("get/{id}")]
+        public Album GetAlbumById(int id)
+        {
+            var data = album.GetAll().Where(p => p.AlbumID == id).FirstOrDefault();
+            return data;
+        }
+
+        [HttpDelete]
+        public string DeleteById(int id)
+        {
+            try
+            {
+                album.DeleteById(id);
+                var i = album.SaveChange();
+                if (i > 0)
+                    return "Delete successfully.";
+                else
+                    return "Delete failed.";
+            }
+            catch (Exception ex)
+            {
+                return "Delete : " + ex.Message;
+            }
+        }
+
+        //[HttpDelete]
+        //public HttpResponseMessage DeleteById(int id)
+        //{
+        //    try
+        //    {
+        //        album.DeleteById(id);
+        //        var i = album.SaveChange();
+        //        if (i > 0)
+        //            return Request.CreateResponse(HttpStatusCode.OK);
+        //        else
+        //            return Request.CreateResponse(HttpStatusCode.BadRequest);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+        //    }
+        //}
+
+        [HttpPut]
+        public string UpdateById(Album album)
+        {
+            try
+            {
+                this.album.Update(album);
+                var i = this.album.SaveChange();
+                if (i > 0)
+                    return "Update successfully.";
+                else
+                    return "Update failed.";
+            }
+            catch (Exception ex)
+            {
+                return "Update : " + ex.Message;
+            }
+        }
+    }
+}
---
+++ MusicStore/API/AuthorController.cs
@@ -0,0 +1,30 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicStore.API
+{
+    public class AuthorController : ApiController
+    {
+        private Repository<Author> author = new Repository<Author>();
+
+        [HttpGet]
+        public List<Author> GetAllAuthor()
+        {
+            var data = author.GetAll();
+            return data;
+
+        }
+
+        [HttpGet]
+        public Author GetAlbumById(int id)
+        {
+            var data = author.GetAll().Where(a => a.AuthorID == id).FirstOrDefault();
+            return data;
+        }
+    }
+}
